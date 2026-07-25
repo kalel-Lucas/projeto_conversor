2df401ea26b93e47b66412ef680adfc46a8ddaf1
Kalel Lucas <kalel.lucas@iesb.edu.br>
2026-07-25 15:32:49 -0300

    Implementando um menu interativo com o usuário!

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import requests 
+def main(): 
+    menu = ''' 
+        ___COTAÇÃO - DÓLAR/REAL___
 
-def converter_moeda (): 
-    url = "https://economia.awesomeapi.com.br/last/USD-BRL"
-    resposta = requests.get(url)
-    dados = resposta.json()
-    
+
+        1. Ver cotação atual 
+        2. Sair 
+    '''
+    rodando = True 
+    while rodando: 
+        print(menu)
+        opcao = input("Digite a opção desejada:").strip()
+        if opcao not in ["1", "2"]: 
+            print("\n\nPor favor, inserir uma opção válida!\n\n")
+        elif opcao == "1": 
+            url = "https://economia.awesomeapi.com.br/last/USD-BRL"
+            resposta = requests.get(url)
+            dados = resposta.json()
+            dolar_atual = dados["USDBRL"]["bid"]
+            print(f"Cotação atual do Dólar->Real brasileiro: R$ {dolar_atual}")
+        elif opcao == "2":
+            print("Até mais :)")
+            break; 
+
+if __name__ == "__main__":
+     main()
